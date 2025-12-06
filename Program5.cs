06d1cb0ef2f7dce1fef3365bb77a1d0d5825a555
Jananisree <167199194+Jananisreesekar@users.noreply.github.com>
2025-12-06 15:58:38 +0530

    Add files via upload

---
+++ Program5.cs
@@ -0,0 +1,24 @@
+﻿//INITIALIZING AN ARRAY
+using System;
+namespace program
+{
+    class sample
+    {
+        static void Main()
+        {
+            int[] a = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Enter the Value: ");
+                int b = int.Parse(Console.ReadLine());
+                a[i] = b;
+            }
+            Console.WriteLine("The values in the array are: ");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+        }
+    }
+}
+
