d1ed7deab97f2e6259d0864e2461d42092ae36e9
Jananisree <167199194+Jananisreesekar@users.noreply.github.com>
2025-12-06 16:02:34 +0530

    Add files via upload

---
+++ Program8.cs
@@ -0,0 +1,28 @@
+﻿//REVERSING THE ARRAY
+using System;
+namespace program
+{
+    class sample
+    {
+        static void Main()
+        {
+            int[] a = new int[5];
+            Console.WriteLine("Enter the values: ");
+            for (int i = 0; i < 5; i++)
+            {
+                a[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("The values of the array are: ");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+            Array.Reverse(a);
+            Console.WriteLine("The reversed array is:");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+        }
+    }
+}
