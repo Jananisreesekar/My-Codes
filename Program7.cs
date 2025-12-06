acd5cbfd29711cbb79455967eed1fb5ab6836ef4
Jananisree <167199194+Jananisreesekar@users.noreply.github.com>
2025-12-06 16:01:09 +0530

    Add files via upload

---
+++ Program7.cs
@@ -0,0 +1,31 @@
+﻿//SORTING THE ARRAYS
+using System;
+namespace program
+{
+    class sample
+    {
+        static void Main()
+        {
+            int[] b = new int[5];
+            Console.WriteLine("Enter the values: ");
+            for (int i = 0; i < 5; i++)
+            {
+                b[i] = int.Parse(Console.ReadLine());
+
+            }
+            Console.WriteLine("The values of the array are: ");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(b[i]);
+            }
+
+            Array.Sort(b);
+            Console.WriteLine("The Sorted array is: ");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(b[i]);
+            }
+        }
+    }
+}
+
