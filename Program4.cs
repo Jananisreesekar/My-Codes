6b48f1c30f07fe65c8119465d87ad2de90e1a21b
Jananisree <167199194+Jananisreesekar@users.noreply.github.com>
2025-12-06 15:55:49 +0530

    Add files via upload

---
+++ Program4.cs
@@ -0,0 +1,18 @@
+﻿//PRINTING THE OUTPUT USING PLACEHOLDER
+using System;
+namespace program
+{
+    class sample
+    {
+        static void Main()
+        {
+            string name = "XXX";
+            Console.WriteLine("My name is: {0}", name);
+            int age = 18;
+            Console.WriteLine("My age is: {0}", age);
+            string favouritesubject = "Science";
+            Console.WriteLine("My favourite subject is: {0}", favouritesubject);
+        }
+    }
+}
+
